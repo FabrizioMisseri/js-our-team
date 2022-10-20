af9ac62c3bff93c514279b744d2684d7036e15fd
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-20 13:35:53 +0200

    milestone 1

--- js/script.js
+++ js/script.js
@@ -10,7 +10,61 @@
 
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
+// FATTO
 // MILESTONE 1:
 // Stampare su console le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+const person1 = {
+    name: "Wayne Barnett",	
+    role: "Founder & CEO",	        
+    photo: "wayne-barnett-founder-ceo.jpg"
+};
+
+const person2 = {
+    name: "Angela Caroll",
+    role: "Chief Editor",	        
+    photo: "angela-caroll-chief-editor.jpg"
+};
+
+const person3 = {
+    name: "Walter Gordon",	
+    role: "Office Manager",	        
+    photo: "walter-gordon-office-manager.jpg"
+};
+
+const person4 = {
+    name: "Angela Lopez",	
+    role: "Social Media Manager",	
+    photo: "angela-lopez-social-media-manager.jpg"
+};
+
+const person5 = {
+    name: "Scott Estrada",
+    role: "Developer",	            
+    photo: "scott-estrada-developer.jpg"
+};
+
+const person6 = {
+    name: "Barbara Ramos",	
+    role: "Graphic Designer",	    
+    photo: "barbara-ramos-graphic-designer.jpg"
+};
+
+const arrayTeam = [
+    person1,
+    person2,
+    person3,
+    person4,
+    person5,
+    person6
+];
+
+for(let i = 0; i < arrayTeam.length; i++) {
+    let person = arrayTeam[i];
+    for(let key in person) {
+        console.log(person[key]);
+    }
+}
